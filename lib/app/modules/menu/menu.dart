dc7da150fac89780ae58e7506be3dbd237374021
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2025-06-13 15:05:36 -0300

    Adiciona menu lateral com itens dinâmicos e funcionalidade de navegação

--- lib/app/modules/menu/menu.dart
+++ lib/app/modules/menu/menu.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+import '../../core/colors.dart';
+import 'menu_itens.dart';
 
 class Menu extends StatefulWidget {
   const Menu({super.key});
@@ -8,18 +11,99 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
+  final controller = MenuItens();
+  int currentIndex = 0;
+  bool selectedItem = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Row(
         children: [
-          Container(
-            width: 200,
-            height: double.infinity,
-            decoration: BoxDecoration(color: const Color(0xFF6001A2)),
+          background(
+            Column(
+              children: [
+                Icon(AntDesign.sketch_outline, size: 150),
+                Expanded(
+                  flex: 3,
+                  child: ListView.builder(
+                    itemCount: controller.items.length,
+                    itemBuilder: (context, index) {
+                      final item = controller.items[index];
+                      selectedItem = currentIndex == index;
+                      return Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 2,
+                          horizontal: 8,
+                        ),
+                        decoration: BoxDecoration(
+                          color: selectedItem
+                              ? AppColors.primaryColor.withValues(alpha: 0.05)
+                              : Colors.transparent,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: ListTile(
+                          leading: Icon(item.icon),
+                          title: Text(item.title),
+                          onTap: () {
+                            setState(() {
+                              currentIndex = index;
+                            });
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ),
+
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ListTile(
+                      leading: Icon(Icons.logout),
+                      title: Text('Logout'),
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: PageView.builder(
+              itemCount: controller.items.length,
+              onPageChanged: (index) {
+                setState(() {
+                  currentIndex = index;
+                });
+              },
+              itemBuilder: (context, index) {
+                return SizedBox(child: controller.items[currentIndex].page);
+              },
+            ),
           ),
         ],
       ),
     );
   }
+}
+
+Widget background(Widget child) {
+  return Container(
+    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+    width: 200,
+    height: double.infinity,
+    decoration: BoxDecoration(
+      color: AppColors.whiteColor,
+      borderRadius: BorderRadius.circular(8),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withValues(alpha: 0.9),
+          blurRadius: 1,
+          spreadRadius: 0,
+        ),
+      ],
+    ),
+    child: child,
+  );
 }
