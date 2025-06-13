684a0e97f4e5e61884ee19519bf24efc94fbebd1
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2025-06-13 15:05:21 -0300

    Refatora a definição de cores, encapsulando constantes na classe AppColors

--- lib/app/core/colors.dart
+++ lib/app/core/colors.dart
@@ -1,3 +1,9 @@
 import 'package:flutter/material.dart';
 
-const primaryColor = Color(0xFF6001A2);
+class AppColors {
+  AppColors._();
+
+  static const primaryColor = Color(0xFF6001A2);
+
+  static const whiteColor = Color(0xFFFFFFFF);
+}
